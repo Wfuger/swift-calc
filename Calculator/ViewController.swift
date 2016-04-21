a89b371c4a074e1a925de5362fb34ba84dfe246a
Wfuger <wfuger@gmail.com>
2016-04-20 21:41:42 -0600

    calculator is typing numbers whoa

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,6 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsTypingANumber: Bool = false
+    
+    @IBAction func updateDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsTypingANumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsTypingANumber = true
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
